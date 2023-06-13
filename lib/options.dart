945e904439e5adec76665898dd6bba92d1500c85
torrfel <juanfe.torres4@gmail.com>
2023-06-13 10:05:54 -0500

    estructura basica de las opciones

--- lib/options.dart
+++ lib/options.dart
@@ -35,16 +35,52 @@ class _optionsState extends State<options> {
             margin: EdgeInsets.only(
               top: 60
             ),
-            padding: EdgeInsets.all(0),
+           
             decoration: BoxDecoration(
-              color: Colors.grey
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.black,
+                  width: 2
+                )
+              ),
+              color: Colors.white
             ),
-            child: Text(
-              "data",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20
-              ),),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Request",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20
+                      ),
+                    ),
+                    Text("availability: 0",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 15
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Container(
+                  width: 45,
+                  height: 45,
+                  margin: EdgeInsets.only(
+                    bottom: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
