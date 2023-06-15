cc5afd43e7878f7ac7e7125b5de4ed7baae8cc64
torrfel <juanfe.torres4@gmail.com>
2023-06-14 19:23:02 -0500

    appbar

--- lib/request/request.dart
+++ lib/request/request.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mejor_en_bici/home/home.dart';
 
 class request extends StatefulWidget {
   const request({super.key});
@@ -14,7 +15,44 @@ class _requestState extends State<request> {
       title: 'Mi App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text(''),
+          actions: [
+            Align(
+              child: Container(
+                width: 300,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: IconButton
+                        (onPressed: (){
+                          Navigator.push(context, 
+                            MaterialPageRoute(
+                              builder:(context) => home() )
+                            );
+                          }, 
+                          icon: Icon(Icons.arrow_back_ios_new_rounded)
+                        ),
+                    ),
+                    
+                    Container(
+                      width: 100,
+                      margin: EdgeInsets.only(
+                        left: 65
+                      ),
+                      child: Text(
+                        "Request",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),
+                      )
+                    ),
+    
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
         body: Text("prueba"),
       )
