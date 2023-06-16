e5b93de61e0cb13aefc9fad1c5e0e9f9209d0dcf
torrfel <juanfe.torres4@gmail.com>
2023-06-16 17:08:34 -0500

    estructura basica del return

--- lib/return/return.dart
+++ lib/return/return.dart
@@ -2,22 +2,81 @@ import 'package:flutter/material.dart';
 
 class Return extends StatefulWidget {
   const Return({super.key});
+  
 
   @override
+  
   State<Return> createState() => _ReturnState();
 }
 
 class _ReturnState extends State<Return> {
+  Widget alignText(Widget text){
+    return Align(
+    alignment: Alignment.bottomLeft,
+    child: text,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mi App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text(''),
+          backgroundColor: Colors.grey,
+          title: Text('Return'),
+          actions: [
+            
+          ],
         ),
-        body: Text("prueba"),
-      )
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFFE15252)
+            ),
+            child: Column(
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      //se llama a una funcion que hacepta un parametro widget para alinearlo a la izquierda
+                      alignText(Text("Recuerda que la calve de tu candado es:",
+                                  style: TextStyle(
+                                    fontSize: 16
+                                ),)),
+                      alignText(Text("2023",
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                )),
+                      alignText(Text("Recurda poner el candado una vez devuelvas la bici",
+                                  style: TextStyle(
+                                    fontSize: 20
+                                  ),))
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 230,
+                  height: 160,
+                  color: Colors.grey,
+                  child: Image.network("https://cdn.dribbble.com/users/1046923/screenshots/11880765/media/db301739244514d6ec26d0dc541a619b.png",
+                            fit: BoxFit.cover,),
+                ),
+                Container(
+                  child: Text("Desliza a la derecha para devolver la bici"),
+                ),
+                Container(
+                  width: 300,
+                  height: 30,
+                  color: Colors.grey,
+                )
+              
+              ],
+              
+            ),
+          ),
+        ),
     );
   }
 }
