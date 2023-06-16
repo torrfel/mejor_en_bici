57481dc8d5dc0cce16af6650b28d82e4131bce62
torrfel <juanfe.torres4@gmail.com>
2023-06-16 17:47:11 -0500

    estilo del return

--- lib/return/return.dart
+++ lib/return/return.dart
@@ -36,41 +36,67 @@ class _ReturnState extends State<Return> {
             child: Column(
               children: [
                 Container(
+                  margin: EdgeInsets.all(20),
                   child: Column(
                     children: [
                       //se llama a una funcion que hacepta un parametro widget para alinearlo a la izquierda
                       alignText(Text("Recuerda que la calve de tu candado es:",
                                   style: TextStyle(
-                                    fontSize: 16
+                                    fontSize: 16,
+                                    color: Colors.white
                                 ),)),
                       alignText(Text("2023",
                                   style: TextStyle(
+                                    color: Colors.white,
                                     fontSize: 24,
                                     fontWeight: FontWeight.bold
                                   ),
                                 )),
                       alignText(Text("Recurda poner el candado una vez devuelvas la bici",
                                   style: TextStyle(
+                                    color: Colors.white,
                                     fontSize: 20
                                   ),))
                     ],
                   ),
                 ),
-                Container(
-                  width: 230,
-                  height: 160,
-                  color: Colors.grey,
-                  child: Image.network("https://cdn.dribbble.com/users/1046923/screenshots/11880765/media/db301739244514d6ec26d0dc541a619b.png",
-                            fit: BoxFit.cover,),
+                Expanded(
+                  child: Container(
+                    width: 230,
+                    height: 160,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(30),
+                      image: DecorationImage(
+                        image:NetworkImage("https://cdn.dribbble.com/users/1046923/screenshots/11880765/media/db301739244514d6ec26d0dc541a619b.png"),
+                        fit: BoxFit.cover
+                      )
+                    ),
+                             
+                  ),
                 ),
-                Container(
-                  child: Text("Desliza a la derecha para devolver la bici"),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(20),
+                    child: Center(
+                      child: Text("Desliza a la derecha para devolver la bici",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-                Container(
-                  width: 300,
-                  height: 30,
-                  color: Colors.grey,
-                )
+                Flexible(
+                  child: Container(
+                    width: 300,
+                    height: 70,
+                    color: Colors.grey,
+                  ),
+                ),
               
               ],
               
