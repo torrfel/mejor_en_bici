7aaf62b1fa208b111d7f196b7f70fc3957069958
torrfel <juanfe.torres4@gmail.com>
2023-06-15 20:02:00 -0500

    Revert "Estilos en la segunda seccion"
    This reverts commit 47e0a2a09cb6da101d2138d3c2db89b19cf70f0d.

--- lib/request/request.dart
+++ lib/request/request.dart
@@ -17,7 +17,7 @@ class _requestState extends State<request> {
       title: 'Mi App',
       home: Scaffold(
         appBar: AppBar( 
-          backgroundColor:Colors.grey,
+          backgroundColor:Color(0xFF04D939),
           
           actions: [
             Align(
